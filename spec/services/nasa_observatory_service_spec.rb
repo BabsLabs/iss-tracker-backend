7a743c3bc0f652b6101039d6812bbcbc14765383
Brian Bower <brian@brianbower.com>
2020-07-25 16:11:47 -0600

    Add tests for NasaObservatoryService response

---
+++ spec/services/nasa_observatory_service_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe NasaObservatoryService do
+  it 'fetches the location of Nasa Observatories from the Nasa API' do
+    nasa_observatory_service = NasaObservatoryService.new
+    
+    expect(nasa_observatory_service).to be_a NasaObservatoryService
+    
+    nasa_observatory_service_response = NasaObservatoryService.observatory_locations
+
+    expect(nasa_observatory_service_response).to have_key :GroundStation
+    expect(nasa_observatory_service_response[:GroundStation]).to be_an Array
+    expect(nasa_observatory_service_response[:GroundStation][0]).to eq("java.util.ArrayList")
+    expect(nasa_observatory_service_response[:GroundStation][1]).to be_an Array
+    expect(nasa_observatory_service_response[:GroundStation][1][0]).to have_key :Id
+    expect(nasa_observatory_service_response[:GroundStation][1][0]).to have_key :Name
+    expect(nasa_observatory_service_response[:GroundStation][1][0]).to have_key :Location
+    expect(nasa_observatory_service_response[:GroundStation][1][0][:Location]).to be_a Hash
+    expect(nasa_observatory_service_response[:GroundStation][1][0][:Location]).to have_key :Latitude
+    expect(nasa_observatory_service_response[:GroundStation][1][0][:Location]).to have_key :Longitude
+
+  end
+end

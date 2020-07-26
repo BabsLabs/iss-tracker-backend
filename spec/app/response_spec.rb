4c92db33a5bb38df6bd8182c6ba8f23f5c4415cb
Brian Bower <brian@brianbower.com>
2020-07-26 15:04:47 -0600

    Split tests into individual endpoint tests

--- spec/app/response_spec.rb
+++ spec/app/response_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-describe '/response endpoint' do
-  it 'the response generates the proper keys' do
-    get '/response'
+describe '/iss endpoint' do
+  it 'the response generates the proper iss location container' do
+    get '/iss'
 
     expect(last_response).to be_ok
     
@@ -11,13 +11,29 @@ describe '/response endpoint' do
     expect(response).to have_key :data
     expect(response[:data]).to have_key :IssLocation
     expect(response[:data][:IssLocation]).to be_a Hash
+  end
+
+  it 'the response generates the proper events container' do
+    get '/events'
+
+    expect(last_response).to be_ok
+    
+    response = JSON.parse(last_response.body, symbolize_names: true)
+
+    expect(response[:data]).to have_key :NasaEvents
+    expect(response[:data][:NasaEvents]).to be_an Array
+  end
+
+  it 'the response generates the proper observatory container' do
+    get '/observatories'
+
+    expect(last_response).to be_ok
+    
+    response = JSON.parse(last_response.body, symbolize_names: true)
 
     expect(response[:data]).to have_key :NasaObservatories
     expect(response[:data][:NasaObservatories]).to be_an Array
     expect(response[:data]).not_to have_key "GroundStation"
     expect(response[:data][:NasaObservatories][1]).not_to have_key "GroundStation"
-
-    expect(response[:data]).to have_key :NasaEvents
-    expect(response[:data][:NasaEvents]).to be_an Array
   end
 end

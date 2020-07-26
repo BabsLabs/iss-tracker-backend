0f1514161e79a6070474073a7097e91b74aedc2d
Brian Bower <brian@brianbower.com>
2020-07-26 15:03:28 -0600

    Update endpoint to split into multiple dedicated endpoints

--- app.rb
+++ app.rb
@@ -6,7 +6,17 @@ get '/' do
   "Hello World!"
 end
 
-get '/response' do
-  response = ResponseService.response
+get '/iss' do
+  response = ResponseService.iss_location_response
+  response.to_json
+end
+
+get '/events' do
+  response = ResponseService.events_response
+  response.to_json
+end
+
+get '/observatories' do
+  response = ResponseService.observatories_response
   response.to_json
 end

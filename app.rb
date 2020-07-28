14e23715fae11956fcf1f5da7934e08a2ad88541
Brian Bower <brian@brianbower.com>
2020-07-28 16:11:19 -0600

    Add before block for request authorization and response CORS header

--- app.rb
+++ app.rb
@@ -3,25 +3,25 @@ require 'sinatra'
 Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| load(f) }
 
 get '/' do
-  response['Access-Control-Allow-Origin'] = '*'
-  "Hello World!"
+  'Hello World!'
 end
 
 get '/iss' do
-  response['Access-Control-Allow-Origin'] = '*'
   response = ResponseService.iss_location_response
   response.to_json
 end
 
 get '/events' do
-  response['Access-Control-Allow-Origin'] = '*'
   response = ResponseService.events_response
   response.to_json
 end
 
 get '/observatories' do
-  response['Access-Control-Allow-Origin'] = '*'
   response = ResponseService.observatories_response
   response.to_json
 end
 
+before do
+  response['Access-Control-Allow-Origin'] = '*'
+  AuthorizationService.authorize(request.env)
+end

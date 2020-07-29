930753dec0da5fc512c4441229914e3ddee8841d
Brian Bower <brian@brianbower.com>
2020-07-28 18:08:06 -0600

    Add error handling

--- app/services/AuthorizationService.rb
+++ app/services/AuthorizationService.rb
@@ -8,10 +8,18 @@ class AuthorizationService
   private 
 
     def self.check_request(request)
-      if request['HTTP_SUPER_SECRET_TOKEN'] == ENV['SUPER_SECRET_TOKEN']
-        true
-      else
-        throw :halt, [ 401, 'Unauthorized - request lacks valid authentication credentials' ]
+      begin 
+        if request['HTTP_SUPER_SECRET_TOKEN'] == ENV['SUPER_SECRET_TOKEN']
+          true
+        else
+          throw :halt, [ 401, 'Unauthorized - request lacks valid authentication credentials' ]
+        end
+      rescue UncaughtThrowError => e
+          puts(e)
+          return false
+      rescue => e
+          puts(e)
+          return false
       end
     end
 end

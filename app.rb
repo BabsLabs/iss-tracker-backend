4766adf15207ee28cd67be23df907885abad0b24
Brian Bower <brian@brianbower.com>
2020-07-29 14:53:51 -0600

    Add authorization for CORS preflight options request

--- app.rb
+++ app.rb
@@ -2,6 +2,16 @@ require 'sinatra'
 
 Dir["#{File.dirname(__FILE__)}/app/**/*.rb"].each { |f| load(f) }
 
+set(:method) do |method|
+  method = method.to_s.upcase
+  condition { request.request_method == method }
+end
+
+before :method => :get do
+  response['Access-Control-Allow-Origin'] = '*'
+  AuthorizationService.authorize(request.env)
+end
+
 get '/iss' do
   response = ResponseService.iss_location_response
   response.to_json
@@ -17,7 +27,9 @@ get '/observatories' do
   response.to_json
 end
 
-before do
-  response['Access-Control-Allow-Origin'] = '*'
-  AuthorizationService.authorize(request.env)
+options '*' do
+  response.headers["Allow"] = "GET, PUT, POST, DELETE, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token, super_secret_token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
 end
